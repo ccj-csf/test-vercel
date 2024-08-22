6e139dfa65b22c256daa8fdb30b5de5a19904010
ccjmlp <ccj@socrates.com>
2024-08-22 21:36:34 +0800

    feat(WalletModal): 重构二次确认弹窗代码，提升代码可读性

--- src/app/(app)/account/components/WalletModal.tsx
+++ src/app/(app)/account/components/WalletModal.tsx
@@ -47,35 +47,7 @@ const WalletModal: React.FC<IWalletModalProps> = ({
 
   return (
     <>
-      {/* 二次确认弹窗 */}
-      <Modal
-        visible={isConfirmVisible}
-        showCloseButton={true}
-        onClose={() => setIsConfirmVisible(false)}
-        className="!z-[2000]"
-        key={Math.random()}
-      >
-        <main className="flex flex-col items-center p-4 px-0">
-          <h2 className="text-xl">Confirm Disconnect</h2>
-          <p className="my-4 text-center ">Are you sure you want to disconnect your wallet?</p>
-          <div className="mt-6 grid grid-cols-2 gap-4">
-            <Button
-              variant="black"
-              className="!h-[50px] !rounded-12 !bg-white !text-15 !text-black"
-              onClick={() => setIsConfirmVisible(false)}
-            >
-              Cancel
-            </Button>
-            <Button
-              variant="black"
-              className="!h-[50px] !rounded-12 !bg-black !text-15 !text-white"
-              onClick={handleConfirmDisconnect}
-            >
-              Disconnect
-            </Button>
-          </div>
-        </main>
-      </Modal>
+
       <Popup visible={visible} showCloseButton={true} onClose={onClose} className="!z-10">
         <main className="flex flex-col items-center pt-4">
           <Image src="/icons/ton.svg" width={64} height={64} alt="ton" />
@@ -105,6 +77,38 @@ const WalletModal: React.FC<IWalletModalProps> = ({
           )}
         </main>
       </Popup>
+            {/* 二次确认弹窗 */}
+            <div   className="!z-[20000]">
+
+<Modal
+  visible={isConfirmVisible}
+  showCloseButton={true}
+  onClose={() => setIsConfirmVisible(false)}
+  className="!z-[20000]"
+  key={Math.random()}
+>
+  <main className="flex flex-col items-center p-4 px-0">
+    <h2 className="text-xl">Confirm Disconnect</h2>
+    <p className="my-4 text-center ">Are you sure you want to disconnect your wallet?</p>
+    <div className="mt-6 grid grid-cols-2 gap-4">
+      <Button
+        variant="black"
+        className="!h-[50px] !rounded-12 !bg-white !text-15 !text-black"
+        onClick={() => setIsConfirmVisible(false)}
+      >
+        Cancel
+      </Button>
+      <Button
+        variant="black"
+        className="!h-[50px] !rounded-12 !bg-black !text-15 !text-white"
+        onClick={handleConfirmDisconnect}
+      >
+        Disconnect
+      </Button>
+    </div>
+  </main>
+</Modal>
+</div>
     </>
   );
 };
