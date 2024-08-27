c685295763fcbdd3226660ff749bf719f39917bb
ccjmlp <ccj@socrates.com>
2024-08-27 18:08:34 +0800

    refactor(actions/earn.ts): 修改updateEarnDataAction函数的注释

--- src/actions/earn.ts
+++ src/actions/earn.ts
@@ -8,9 +8,9 @@ export async function getEarnDataAction() {
 }
 
 // 更新数据，返回新的数据
-export async function updateEarnDataAction<T extends IEarnDataType>(
-  dataType: T,
-  updatePayload: UpdatePayload<T>,
-) {
-  return updateEarnData({ type: dataType, data: updatePayload });
-}
+// export async function updateEarnDataAction<T extends IEarnDataType>(
+//   dataType: T,
+//   updatePayload: UpdatePayload<T>,
+// ) {
+//   return updateEarnData({ type: dataType, data: updatePayload });
+// }
